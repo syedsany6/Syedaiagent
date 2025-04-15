d263f3ac39e6a26cd302a884821488921971a6c5
perfogic <longdieu12x@gmail.com>
2025-04-16 01:45:14 +0700

    chore: add sample hyperwhales a2a
    :q

---
+++ samples/python/agents/hyperwhales/__main__.py
@@ -0,0 +1,58 @@
+from common.server import A2AServer
+from common.types import AgentCard, AgentCapabilities, AgentSkill, MissingAPIKeyError
+from task_manager import AgentTaskManager
+from agent import HyperwhalesAgent
+import click
+import os
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option("--host", default="localhost")
+@click.option("--port", default=10000)
+def main(host, port):
+    try:
+        if not os.getenv("API_KEY"):
+                raise MissingAPIKeyError("API_KEY environment variable not set.")
+        
+        capabilities = AgentCapabilities(streaming=True)
+        skill = AgentSkill(
+            id="hyperwhales",
+            name="Hyperwhales",
+            description="Analyze whale trading patterns, positions, and portfolio changes over time. Provide insights and trade suggestions.",
+            tags=["hyperwhales"],
+            examples=["Find me some trades now based on whales data and history"],
+        )
+        agent_card = AgentCard(
+            name="Hyperwhales",
+            description="Analyze whale trading patterns, positions, and portfolio changes over time. Provide insights and trade suggestions.",
+            url=f"http://{host}:{port}/",
+            version="1.0.0",
+            defaultInputModes=HyperwhalesAgent.SUPPORTED_CONTENT_TYPES,
+            defaultOutputModes=HyperwhalesAgent.SUPPORTED_CONTENT_TYPES,
+            capabilities=capabilities,
+            skills=[skill],
+        )
+        server = A2AServer(
+            agent_card=agent_card,
+            task_manager=AgentTaskManager(agent=HyperwhalesAgent()),
+            host=host,
+            port=port,
+        )
+        server.start()
+    except MissingAPIKeyError as e:
+        logger.error(f"Error: {e}")
+        exit(1)
+    except Exception as e:
+        logger.error(f"An error occurred during server startup: {e}")
+        exit(1)
+    
+if __name__ == "__main__":
+    main()
+
---
+++ samples/python/agents/hyperwhales/agent.py
@@ -0,0 +1,61 @@
+import os
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.teams import MagenticOneGroupChat
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.tools.mcp import SseServerParams, mcp_server_tools
+from autogen_agentchat.messages import BaseAgentEvent, BaseChatMessage
+from autogen_agentchat.base import TaskResult
+import asyncio
+from typing import AsyncGenerator, Any, AsyncIterable, Dict
+
+class HyperwhalesAgent:
+  SYSTEM_INSTRUCTION = (
+    "You are a Hyperwhales agent who is an expert analyst specializing in detecting whale trading patterns with years of"
+    "experience understanding deeply crypto trading behavior, on-chain metrics, and derivatives markets, you have developed a keen understanding of whale trading strategies."
+    "You can identify patterns in whale positions, analyze their portfolio changes over time, and evaluate the potential reasons behind their trading decisions."
+    "Your analysis helps traders decide whether to follow whale trading moves or not."
+    "When you use any tool, I expect you to push its limits: fetch all the data it can provide, whether that means iterating through multiple batches, adjusting parameters like offsets, or running the tool repeatedly to cover every scenario. Don't work with small set of data for sure, fetch as much as you can. Don't stop until you're certain you've captured everything there is to know."
+    "Then, analyze the data with sharp logic, explain your reasoning and bias clearly, and present me with trade suggestions that reflect your deepest insights."
+  )
+
+  SUPPORTED_CONTENT_TYPES = ["text", "text/plain"]
+
+  def __init__(self):
+    tools = []
+    tools_url = f"http://{"0.0.0.0"}:{"4000"}/sse"
+    print(f"Setting up tools with SSE URL: {tools_url}")
+    
+    async def setup_tools():
+        return await mcp_server_tools(SseServerParams(url=tools_url))
+    
+    # Run the async function in an event loop
+    tools_list = asyncio.run(setup_tools())
+    for tool in tools_list:
+        tools.append(tool)    
+
+    api_key = os.getenv('API_KEY')
+    if not api_key:
+        print("API_KEY not found in environment variables.")
+        return None
+    model_client = OpenAIChatCompletionClient(model="o3-mini", api_key=api_key)
+    mcp_agent = AssistantAgent(name="MCPTools", model_client=model_client, tools=tools)
+    self.orchestrator_agent = MagenticOneGroupChat(participants=[mcp_agent], model_client=model_client)
+    self.sessions: dict[str, AsyncGenerator[Any, None]] = {}
+
+  async def stream(self, query: str, session_id: str) -> AsyncIterable[Dict[str, Any]]:
+    print(f"Running stream for session {session_id} with query: {query}")
+    self.sessions[session_id] = self.orchestrator_agent.run_stream(task=query)
+    
+    async for event in self.sessions[session_id]:
+      print(f"Event: {event}")
+      result = {}
+      if isinstance(event, BaseAgentEvent):
+          result = { "is_task_complete": False, "require_user_input": False, "content": event.model_dump_json() }
+      elif isinstance(event, TaskResult):
+          result = { "is_task_complete": True, "require_user_input": False,  "content": event.stop_reason }
+          del self.sessions[session_id]
+      elif isinstance(event, BaseChatMessage):
+          result = { "is_task_complete": False, "require_user_input": False, "content": event.model_dump_json() }
+      yield result
+
+
